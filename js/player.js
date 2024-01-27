4754e10f21ebad354455b096a7c16a8c1d34c04a
Oleksandr Zheliabin <oleksandr.zheliabin@gmail.com>
2024-01-26 22:40:40 -0800

    New commit

--- js/player.js
+++ js/player.js
@@ -18,34 +18,55 @@ class Jukebox {
 }
 
 class Album {
-    constructor(artist, title) {
-        this.artist = artist
-        this.title = title
+    constructor(name) {
+        this.name = name
         this.played = 0
     }
     play = function() {
         this.played += 1
     }
     display = function() {
-        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
+        return `${this.name}. The album has been played ${this.played} times.`
     }
 }
 
 var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+// Store artists/albums within an array
+const albums = ['Operation Ivy - Energy', 'Blink 182 - Dude Ranch', 'New Found Glory - Sticks and Stones']
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+// Bind that array to the drop down menu on the web page
+let dropdow = document.getElementById('artAlbums')
+let options=''
+albums.map((op,i)=>{
+         options+=`<option value='${op}' id='${i}'>${op}</option>`
+      })
+console.log(options)
+dropdow.innerHTML = options
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+// In code, iterate through the array and programmatically create new Album class instances based on each album within the array. 
+// You’ll also need to add these instances to the player.
+albumsList = []
+for (let i = 0; i < albums.length; i++) {
+
+   // albumsList[i] = new Album(albums[i])
+    window["album"+i] = new Album(albums[i]);
+    jbox.addAlbum(window["album"+i])
+
+}
+
+// Add a "play" button to the web page. Add “show my favorite album” button
+
+const playBtn = document.getElementById("play");
+const favBtn = document.getElementById("favorite");
+
+playBtn.addEventListener("click", playButton);
+function playButton() {
+    window['album' + document.getElementById('artAlbums').selectedIndex].play()
+    console.log(`Play album ${document.getElementById('artAlbums').value}`)
+}
+favBtn.addEventListener("click", favButton);
+
+function favButton() {
+    document.getElementById('favoriteAlbum').innerHTML = `Your favorite album is: ${jbox.favoriteAlbum()}`
+}
